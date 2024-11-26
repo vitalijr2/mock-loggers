b576bd2ffd18c4197e18f0e3ec51125e2d717f73
Vitalij Berdinskih <vitalij_r2@outlook.com>
2024-11-26 13:43:40 +0200

    service provider test

--- tinylog-provider/src/test/java/io/github/vitalijr2/logging/mock/tinylog/MockLoggerProviderSlowTest.java
+++ tinylog-provider/src/test/java/io/github/vitalijr2/logging/mock/tinylog/MockLoggerProviderSlowTest.java
@@ -1,14 +1,22 @@
 package io.github.vitalijr2.logging.mock.tinylog;
 
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.isNull;
+import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.clearInvocations;
 import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.tinylog.Level;
+import org.tinylog.Logger;
 
 @Tag("slow")
 class MockLoggerProviderSlowTest {
@@ -26,7 +34,6 @@ class MockLoggerProviderSlowTest {
     reset(MockLoggerProvider.MOCK_INSTANCE);
   }
 
-  // Move to slow
   @DisplayName("Clear and reset the mock instance")
   @Test
   void cleanAndReset() throws InterruptedException {
@@ -36,6 +43,22 @@ class MockLoggerProviderSlowTest {
 
     // then
     verifyNoInteractions(MockLoggerProvider.MOCK_INSTANCE);
+  }
+
+  @DisplayName("Service provider")
+  @Test
+  void serviceProvider() {
+    // given
+    when(MockLoggerProvider.MOCK_INSTANCE.getMinimumLevel(isNull())).thenReturn(Level.INFO);
+
+    // when
+    Logger.info("test message");
+
+    // then
+    verify(MockLoggerProvider.MOCK_INSTANCE, atLeastOnce()).getMinimumLevel(isNull());
+    verify(MockLoggerProvider.MOCK_INSTANCE).log(eq(Level.INFO.ordinal()), isNull(), eq(Level.INFO), isNull(), isNull(),
+        eq("test message"), isNull());
+    verifyNoMoreInteractions(MockLoggerProvider.MOCK_INSTANCE);
   }
 
 }
